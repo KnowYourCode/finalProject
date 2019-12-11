d8ed75ca0fe2aee2920d4065368ef0cae14e6a4f
cgutierrez7 <chris.n.gutierrez@gmail.com>
2019-12-11 13:14:39 -0800

    timer and dad joke api

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const superagent = require('superagent');
 const vscode = require('vscode');
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,9 +21,13 @@ function activate(context) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
-
+		timeTracker(context);
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Know Your Code is active.');
+	});
+
+	let dadJoke = vscode.commands.registerCommand('extension.dadJoke', function() {
+		dadJokeRetriever();
 	});
 
 	context.subscriptions.push(disposable);
@@ -30,6 +36,21 @@ exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
+
+function timeTracker(context) {
+		const sessionStartTime = Date.now();
+		console.log(Math.floor((sessionStartTime - sessionStartTime)/1000) + "seconds");
+		context.globalState.update('start', sessionStartTime);
+		console.log(context.globalState.get('start'));
+}
+
+function dadJokeRetriever() {
+	superagent
+	.get('https://icanhazdadjoke.com/')
+	.set('Accept', 'application/json')
+	.then(res => console.log(JSON.stringify(res.body.joke)))
+	.catch(error => console.error(error));
+}
 
 module.exports = {
 	activate,
